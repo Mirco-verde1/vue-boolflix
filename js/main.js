988f8a2e356bcb646a16916d3fca815a6ee35011
mirco verderosa <verderosamircowork@gmail.com>
2021-02-10 19:08:49 +0100

    methods con altra chiamata axos per dati su serie tv

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ new Vue({
 
   data:{
     allFilms:[],
+    allTvSeries:[],
     userInputSearch:'',
     moviesRating:0,
     moviesLanguagesList:[
       'en',
       'it',
       'es',
-    '  ja',
+      'ja',
       'de',
       'sv',
       'fr',
@@ -22,22 +23,34 @@ new Vue({
   },
 
   methods:{
+    //dopo aver effettuato la mia richiesta con axos,salvo i dati---------------------------------->
     researchMovieAndRating:function(){
       const self = this;
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
 
       .then(function(resp) {
-        const dataResult = resp.data.results
-        self.allFilms = dataResult;
+        self.allFilms = resp.data.results
         console.log(self.allFilms) //allFilms sarà il mio contenitore dati
       })
+    },
 
-    },//successivamente  trasformo il voto di ogni film da decimale ad intero,arrotondato per eccesso
+    //successivamente  trasformo il voto di ogni film da decimale ad intero,arrotondato per eccesso
     movieRatingInteger:function(element){
     this.moviesRating = parseInt(Math.ceil(element / 2));
     return this.moviesRating;
-
   },
+
+//modifico la mia chiamata per estrapolare dai dati anche le serie tv---------------------------------------->
+  researchTvSeriesAndRating:function(){
+    const self = this;
+    axios.get("https://api.themoviedb.org/3/search/tv?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
+
+    .then(function(resp) {
+      self.allTvSeries = resp.data.results
+      console.log(self.allTvSeries);
+
+    })
+  }
 
 
   }
