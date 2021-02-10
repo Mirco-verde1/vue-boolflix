70078bc982d319ee90eb0539190ab179d1ea89cd
mirco verderosa <verderosamircowork@gmail.com>
2021-02-10 12:57:22 +0100

    stampata a schermo votazione del film

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
 new Vue({
-el:'#BoolFlix',
+  el:'#BoolFlix',
 
-data:{
-  allFims:[],
-  userInputSearch:''
-},
+  data:{
+    allFilms:[],
+    userInputSearch:'',
+    moviesRating:0
 
-mounted(){
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70')
-},
+  },
 
-methods:{
-  researchByInput:function(){
-  const self = this;
-    axios.get("https://api.themoviedb.org/3/search/movie?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
-    .then(function(resp) {
-      const dataResult = resp.data.results
-      self.allFims = dataResult;
-      console.log(self.allFims);
+  mounted(){
 
-})
+  },
+
+  methods:{
+    researchMovieAndRating:function(){
+      const self = this;
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
+
+      .then(function(resp) {
+        const dataResult = resp.data.results
+        self.allFilms = dataResult;
+        console.log(self.allFilms) //allFilms sarà il mio contenitore dati
+
+
+      })
+
+    },//successivamente  trasformo il voto di ogni film da decimale ad intero,arrotondato per eccesso
+    movieRatingInteger:function(element){
+    this.moviesRating = parseInt(Math.ceil(element / 2));
+    return this.moviesRating;
+
+    }
+
 
   }
-}
 
 
 })
