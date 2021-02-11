4d617e20930f7bb9a95a5f434a143af9be2069cc
mirco verderosa <verderosamircowork@gmail.com>
2021-02-11 15:06:48 +0100

    inizio lavoro layout

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ new Vue({
     userInputSearch:'',
     moviesRating:0,
     urlImages:'http://image.tmdb.org/t/p/w500/',
-    moviesLanguagesList:[
+    moviesLangList:[
       'en',
       'it',
       'es',
@@ -18,20 +18,15 @@ new Vue({
       'no'
     ],
   },
+methods:{
 
-  mounted(){
-
-  },
-
-  methods:{
-    //dopo aver effettuato la mia richiesta con axos,salvo i dati---------------------------------->
-    researchMovieAndRating:function(){
+  //dopo aver effettuato la mia richiesta con axos,salvo i dati---------------------------------->
+    researchMovie:function(){
       const self = this;
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
 
       .then(function(resp) {
         self.allFilms = resp.data.results
-        console.log(self.allFilms) //allFilms sarà il mio contenitore dati
       })
     },
 
@@ -41,17 +36,16 @@ new Vue({
     return this.moviesRating;
   },
 
-//modifico la mia chiamata per estrapolare dai dati anche le serie tv---------------------------------------->
-  researchTvSeriesAndRating:function(){
+//chiamata per estrapolare dai dati anche le serie tv---------------------------------------->
+  researchTvSeries:function(){
     const self = this;
     axios.get("https://api.themoviedb.org/3/search/tv?api_key=423a9bf9c8fcbaa0c7d6d90fe0bffe70&query=" + self.userInputSearch)
 
     .then(function(resp) {
       self.allTvSeries = resp.data.results
-      console.log(self.allTvSeries);
 
     })
-  }
+  },
 
 
   }
